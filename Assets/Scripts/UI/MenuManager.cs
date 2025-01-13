d2f09894906cd7468fa8c7e389c139fd390aa502
Gianluca Colella <colella.gianluca94@gmail.com>
2025-01-13 19:15:59 +0100

    Fixed audiomanager script

--- Assets/Scripts/UI/MenuManager.cs
+++ Assets/Scripts/UI/MenuManager.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
-public class MenuManager : MonoBehaviour
+public class MenuManager : MonoBehaviour, IPointerEnterHandler
 {
     public void ExitButton(){
         Application.Quit();
@@ -16,5 +18,11 @@ public class MenuManager : MonoBehaviour
 
     public void StartButton(){
         SceneManager.LoadScene("Game");
+    }
+
+    public void OnPointerEnter(PointerEventData pointerEventData)
+    {
+        // play sound when mouse hover a btn
+        FindObjectOfType<AudioManager>().Play("OnHoverMenu");
     }
 }
